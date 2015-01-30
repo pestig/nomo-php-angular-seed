8c1fb12aa604c1ffd3e1a691668be9df59c6b5ad
Pesti György <pesti@nomo.hu>
2015-01-30 19:05:07 +0100

    rem grid

--- nomoEFW/app/modules/admin_menu/module.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-angular.module('nomoEFW.admin_menu', [])
--- nomoEFW/app/modules/app.js
+++ nomoEFW/app/modules/app.js
@@ -2,13 +2,10 @@
 
 angular.module('nomoEFW', [
     'ngRoute',
-    'nomoEFW.calendar',
     'nomoEFW.common',
     'nomoEFW.form',
-    'nomoEFW.grid',
 	'nomoEFW.table',
     'nomoEFW.report',
-    'nomoEFW.admin_menu',
     'nomoEFW.modal',
     'nomoEFW.session',
     'nomoEFW.pagination',
