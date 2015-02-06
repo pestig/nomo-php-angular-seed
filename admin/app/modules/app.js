04a74f8f0c211908433ddcad2fce53cd067a4af1
Pesti György <pesti@nomo.hu>
2015-02-06 16:37:01 +0100

    grunt

--- admin/app/modules/app.js
+++ admin/app/modules/app.js
@@ -9,7 +9,7 @@ angular.module('nomoAdmin', [
 		$locationProvider.html5Mode(true);
 		$routeProvider.when('/admin/report/:type', {templateUrl: '/nomoEFW/app/modules/report/view.html',reloadOnSearch: false});
 		$routeProvider.when('/admin/:type/:id', {template: '<div nomo-form nomo-form-class="routeParams.type" nomo-form-id="routeParams.id"></div>',reloadOnSearch: false});
-		$routeProvider.when('/admin/:type', {template: '<div nomo-table nomo-table-class="routeParams.type"></div>',reloadOnSearch: false});
+		$routeProvider.when('/admin/:type', {template: '<div nomo-table nomo-table-class="routeParams.type" nomo-table-template-url="/admin/app/modules/table/view.html"></div>',reloadOnSearch: false});
 		$routeProvider.when('/admin', {redirectTo : '/admin/Peldatabla'});
 		$routeProvider.otherwise({redirectTo: '/admin'});
 	}])
--- nomoEFW/api/index.php
+++ nomoEFW/api/index.php
@@ -14,28 +14,6 @@ try{
       "method" => nomo::$requestURIPathArray[3],
       "params" => $params
     );
-  // http://nomo.hu/api/datatables/server_processing/
-  }else if(count(nomo::$requestURIPathArray)>=3 && in_array(nomo::$requestURIPathArray[1],array("datatables"))){
-    //pre_print_r(nomo::$requestURIPathArray[2]);exit;
-    //include(__DIR__."/server_processing.php");
-
-    $result=NomoAPI::get_datatables_select_params(nomo::$requestURIPathArray[2]);
-    
-    $req=array(
-      "cmd" => "execute",
-      "className" => nomo::$requestURIPathArray[2],
-      "method" => "select",
-      "params" => $result["selectParams"]
-    );
-    $resp=call_user_func(array("NomoAPI",$req["cmd"]),$req); 
-    $rows=$resp["data"];
-    $output=$result["output"];
-
-    $fields=$result["fields"];
-    $output['data']=$rows;
-    
-  	echo json_encode( $output );
-    exit;
   }else{
     $params=array_merge(nomo::$_POST,nomo::$_GET);
     if(isset($params["json"])){
@@ -50,9 +28,6 @@ try{
       throw new Exception("Hiányzó paraméter: 'json'");
     }  
     $req["cmd"]="execute";
-    if(!method_exists('NomoAPI',$req["cmd"])) 
-      throw new Exception("Hibás 'cmd' paraméter: ".$req["cmd"]);
-  
     $notrace=$req["notrace"];
   } 
    
@@ -88,13 +63,15 @@ if($resp["ret"]!=0){
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 
-if($params["responseFormat"]=="jsonInHTMLBody"){ 
+/*if($params["responseFormat"]=="jsonInHTMLBody"){
   header('Content-type: text/html');
   echo "<HTML><BODY>".json_encode($resp)."</BODY></HTML>";
 }elseif($params["responseFormat"]=="javascript"){
   header('Content-type: text/html');
   echo $params["cbFunctionName"]."(".json_encode($resp).")";
-}elseif($params["responseFormat"]=="text"){
+}else
+*/
+if($params["responseFormat"]=="text"){
   header('Content-type: text/plain');
   echo $resp["data"];
 }else{
